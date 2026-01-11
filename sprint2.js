f8e638d520a32a4670a5201dd5089f8c1bb93f95
MarceloDono <marcelo@MacBook-Air-de-Marcelo.local>
2026-01-11 08:31:13 +0100

    ejercicio 2.1. terminado, por hacer, limpiar documento para entregar

--- sprint2.js
+++ sprint2.js
@@ -355,5 +355,17 @@ async function greet(){
   }
 }
 
-/*6 - TRY/CATCH */
+/*6 - PROMISE.ALL */
 
+const p1 = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve("I m P1", 2000);
+  });
+});
+const p2 = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve("I m P2", 3000);
+  });
+});
+
+Promise.all([p1, p2]).then(results => {console.log(results)})
